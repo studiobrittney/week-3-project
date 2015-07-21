07b8cef3d9be146a4dd669e53020ba2bab5f4326
Brittney Takeeta <brittneytaketa@gmail.com>
2015-07-21 18:36:30 -0500

    Work in Progress

---
+++ js/script.js
@@ -0,0 +1,71 @@
+//////////// BOOKS
+
+var books = [
+  book1 = {
+    id:"book1",
+    name:"JavaScript and JQuery: Interactive Front-End Web Development",
+    by:"Jon Duckett",
+    category:"books",
+    price:"$28.48",
+    description:"This book will teach you how to make your websites more interactive, engaging, and usable. It does this by combining programming theory with examples that demonstrate how JavaScript and jQuery are used on popular sites. In no time at all you will be able to think and code like a programmer.",
+    pictureUrl:"img/book1.jpg",
+    sellingPoint:[],
+  },
+
+  book2 = {
+    id:"book2",
+    name:"CSS3: The Missing Manual",
+    by:"David Sawyer McFarland",
+    category:"books",
+    price:"$28.48",
+    description:"CSS3 lets you create professional-looking websites, but learning its finer points can be tricky—even for seasoned web developers. This Missing Manual shows you how to take your HTML and CSS skills to the next level, with valuable tips, tricks, and step-by-step instructions. You’ll quickly learn how to build web pages that look great and run fast on devices and screens of all sizes.",
+    pictureUrl:"img/book2.jpg",
+    sellingPoint:[],
+  },
+
+  book3 = {
+    id:"book3",
+    name:"HTML and CSS: Design and Build Websites",
+    by:"Jon Duckett",
+    category:"books",
+    price:"$29.99",
+    description:"A full-color introduction to the basics of HTML and CSS from the publishers of Wrox! Every day, more and more people want to learn some HTML and CSS. Joining the professional web designers and programmers are new audiences who need to know a little bit of code at work (update a content management system or e-commerce store) and those who want to make their personal blogs more attractive.",
+    pictureUrl:"img/book3.jpg",
+    sellingPoint:[],
+}]
+
+//////////////////////// MUSIC /////////////////////////////
+
+var music = [
+  album1 = {
+    id:"album1",
+    name:"Blown Away",
+    by:"Carrie Underwood",
+    category:"music",
+    price:"$11.99",
+    description:"Produced by Mark Bright, the forthcoming album, Blown Away, was featured in Entertainment Weekly's recent \"10 albums we can't wait for.\" This will be the fourth studio album on 19 Recordings/Arista Nashville for the five-time Grammy® winner. The debut single \"Good Girl\" isco-written by Underwood.",
+    pictureUrl:"img/album1.jpg",
+    sellingPoint:[],
+  },
+
+  album2 = {
+    id:"album2",
+    name:"Red",
+    by:"Taylor Swift",
+    category:"music",
+    price:"$12.49",
+    description:"Red touches on Swift's signature themes of love and heartbreak, however, from a more mature perspective while exploring other themes such as fame and the pressure of being in the limelight. The album features collaborations with producers and guest artists such as Gary Lightbody of Snow Patrol and Ed Sheeran and is noted for Swift's experimentation with new musical genres.",
+    pictureUrl:"img/album2.jpg",
+    sellingPoint:[],
+  },
+
+  album3 = {
+    id:"album3",
+    name:"Same Trailer Different Park",
+    by:"Kacey Musgraves",
+    category:"music",
+    price:"$9.99",
+    description:"After listening to Same Trailer Different Park, the Nashville-based singer-songwriter s first album for Mercury Records, it s clear that this is a girl who has something to say. A true language artist, Kacey nimbly spins webs of words to create the quirky puns, shrewd metaphors, and steely ironies that fill the record.",
+    pictureUrl:"img/album3.jpg",
+    sellingPoint:[],
+}]
